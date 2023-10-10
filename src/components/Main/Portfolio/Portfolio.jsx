4fead2b8163162819a57d32ab0643d00c1d0fb3f
AMG <vitos162008@yandex.ru>
2023-10-10 20:09:19 +0300

    Update portfolio links

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -7,19 +7,19 @@ export default function Portfolio() {
       <ul className="portfolio__links">
         <li className="portfolio__link">
           Статичный сайт
-          <a href="/" aria-label="Статичный сайт" target="_blank" className="portfolio__link-symbol">
+          <a href="https://github.com/vitka27/how-to-learn" aria-label="Статичный сайт" target="_blank" className="portfolio__link-symbol" rel="noreferrer">
             ↗
           </a>
         </li>
         <li className="portfolio__link">
           Адаптивный сайт
-          <a href="/" aria-label="Адаптивный сайт" target="_blank" className="portfolio__link-symbol">
+          <a href="https://github.com/vitka27/russian-travel" aria-label="Адаптивный сайт" target="_blank" className="portfolio__link-symbol" rel="noreferrer">
             ↗
           </a>
         </li>
         <li className="portfolio__link">
           Одностраничное приложение
-          <a href="/" aria-label="Одностраничное приложение" target="_blank" className="portfolio__link-symbol">
+          <a href="https://github.com/vitka27/react-mesto-api-full-gha" aria-label="Одностраничное приложение" target="_blank" className="portfolio__link-symbol" rel="noreferrer">
             ↗
           </a>
         </li>
