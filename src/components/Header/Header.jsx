397d6dd369aa266d59e75b25051b08cb921acaac
AMG <vitos162008@yandex.ru>
2023-10-23 22:54:13 +0300

    Fix style issues in the Header, AboutProject, and NavigationAuthMenu components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,10 @@ import { useState } from "react";
 
 export default function Header({ isAuth }) {
   const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
+  const location = useLocation().pathname;
 
   return (
-    <header className={useLocation().pathname === "/" ? 'wrapper__section wrapper__section_theme_blue header' : 'wrapper__section wrapper__section_theme_dark header'}>
+    <header className={location === "/" ? 'wrapper__section wrapper__section_theme_blue header' : 'wrapper__section wrapper__section_theme_dark header'}>
       <div className=" header__container wrapper__section-container">
         <Logo />
         <Navigation isAuth={isAuth} isOpenBurgerMenu={isOpenBurgerMenu} />
--- src/components/Navigation/NavigationAuthMenu/NavigationAuthMenu.jsx
+++ src/components/Navigation/NavigationAuthMenu/NavigationAuthMenu.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function NavigationAuthMenu({ isAuth }) {
+  const location = useLocation().pathname;
+
   return isAuth ? (
-    <Link to="/profile" className="navigation-auth-menu__profile-link">
-      Акканут
-      <span className="navigation-auth-menu__profile-img" />
-    </Link>
+    <div className="navigation-auth-menu">
+      <Link to="/profile" className="navigation-auth-menu__profile-link">
+        Акканут
+        <span
+          className={
+            location === "/"
+              ? "navigation-auth-menu__profile-img"
+              : "navigation-auth-menu__profile-img navigation-auth-menu__profile-img_bg-color-blue"
+          }
+        />
+      </Link>
+    </div>
   ) : (
     <ul className="navigation-auth-menu">
       <li className="navigation-auth-menu__item">
