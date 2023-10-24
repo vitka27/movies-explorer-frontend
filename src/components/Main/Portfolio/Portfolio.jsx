ca7a9fec31ec2540daab554a64c95469f185d20c
AMG <vitos162008@yandex.ru>
2023-10-24 20:24:28 +0300

    fix portfolio links

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -7,21 +7,39 @@ export default function Portfolio() {
         <h2 className="portfolio__title">Портфолио</h2>
         <ul className="portfolio__links">
           <li className="portfolio__link">
-            Статичный сайт
-            <a href="https://github.com/vitka27/how-to-learn" aria-label="Статичный сайт" target="_blank" className="portfolio__link-symbol" rel="noreferrer">
-              ↗
+            <a
+              href="https://github.com/vitka27/how-to-learn"
+              aria-label="Статичный сайт"
+              target="_blank"
+              className="portfolio__link-url"
+              rel="noreferrer"
+            >
+              <p className="portfolio__link-title">Статичный сайт</p>
+              <span className="portfolio__link-symbol">↗</span>
             </a>
           </li>
           <li className="portfolio__link">
-            Адаптивный сайт
-            <a href="https://github.com/vitka27/russian-travel" aria-label="Адаптивный сайт" target="_blank" className="portfolio__link-symbol" rel="noreferrer">
-              ↗
+            <a
+              href="https://github.com/vitka27/russian-travel"
+              aria-label="Адаптивный сайт"
+              target="_blank"
+              className="portfolio__link-url"
+              rel="noreferrer"
+            >
+              <p className="portfolio__link-title">Адаптивный сайт</p>
+              <span className="portfolio__link-symbol">↗</span>
             </a>
           </li>
           <li className="portfolio__link">
-            Одностраничное приложение
-            <a href="https://github.com/vitka27/react-mesto-api-full-gha" aria-label="Одностраничное приложение" target="_blank" className="portfolio__link-symbol" rel="noreferrer">
-              ↗
+            <a
+              href="https://github.com/vitka27/react-mesto-api-full-gha"
+              aria-label="Одностраничное приложение"
+              target="_blank"
+              className="portfolio__link-url"
+              rel="noreferrer"
+            >
+              <p className="portfolio__link-title">Одностраничное приложение</p>
+              <span className="portfolio__link-symbol">↗</span>
             </a>
           </li>
         </ul>
