a69b4a2d183b48a9875ea7b261c4490f8c31da7b
AMG <vitos162008@yandex.ru>
2023-11-02 00:59:08 +0300

    [Input] Update error message handling logic

--- src/components/Form/Input/Input.jsx
+++ src/components/Form/Input/Input.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-export default function Input({ inputName, error, isEmpty, isDirty,  ...props}) {
+export default function Input({
+  inputName,
+  error,
+  isEmpty,
+  isDirty,
+  ...props
+}) {
   return (
     <div className="input">
       <label htmlFor={props.name} className="input__label">
         {inputName}
       </label>
       <input className="input__text" id={props.name} {...props} />
-      <span className="input__error">{isDirty && isEmpty[props.name] && 'Поле не может быть пустым'}</span>
+      <span className="input__error">
+        {isDirty && isEmpty[props.name] ? "Поле не может быть пустым" : error}
+      </span>
     </div>
   );
 }
