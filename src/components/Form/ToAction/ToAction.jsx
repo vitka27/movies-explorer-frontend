2f8addffca20c430f043cd7c20e8579ba1f1c903
Work PC <vitos162008@mail.ru>
2023-11-14 16:47:33 +0300

    removed the extra wrapper

--- src/components/Form/ToAction/ToAction.jsx
+++ src/components/Form/ToAction/ToAction.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function ToAction({ actionTitle, nameButton, patch }) {
   return (
-    <>
-      <div className="to-action">
-        <p className="to-action__title">{actionTitle}</p>
-        <Link to={patch} className="to-action__register-link">{nameButton}</Link>
-      </div>
-    </>
+    <div className="to-action">
+      <p className="to-action__title">{actionTitle}</p>
+      <Link to={patch} className="to-action__register-link">
+        {nameButton}
+      </Link>
+    </div>
   );
 }
