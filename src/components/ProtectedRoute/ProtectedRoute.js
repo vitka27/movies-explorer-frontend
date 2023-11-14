8273e1c06aa395f14869c97c270c54f2a9f26f47
Work PC <vitos162008@mail.ru>
2023-11-14 13:36:54 +0300

    fixed the redirect to protected

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function ProtectedRoute({ isAuthorezed }) {
-  return isAuthorezed ? <Outlet /> : <Navigate to="/signin" replace />;
+  return isAuthorezed ? <Outlet /> : <Navigate to="/" replace />;
 }
